e11d88b281cf5a5de3832d540c9ca2d065c26327
samanthasampath <samanthasampath64@gmail.com>
2019-06-29 00:26:05 +0530

    make quantity to cart

--- app/src/main/java/com/example/pizzaloop/Details.java
+++ app/src/main/java/com/example/pizzaloop/Details.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,12 +13,14 @@ import android.widget.Toast;
 import com.squareup.picasso.Picasso;
 
 public class Details extends AppCompatActivity {
+    private EditText Amount;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+        Amount=(EditText) findViewById(R.id.etamount);
         Intent intent = getIntent();
 
         TextView name = (TextView) findViewById(R.id.tvname);
@@ -25,9 +28,27 @@ public class Details extends AppCompatActivity {
         TextView details = (TextView) findViewById(R.id.tvdes);
         details.setText(intent.getStringExtra("details"));
         TextView price = (TextView) findViewById(R.id.tvpris);
-        price.setText("Rs."+intent.getFloatExtra("price",0) );
+        price.setText("Unit Price:Rs."+intent.getFloatExtra("price",0) );
 
         ImageView image=(ImageView) findViewById(R.id.ivimg);
         Picasso.get().load(intent.getStringExtra("imgurl")).into(image);
+        System.out.println(intent.getStringExtra("imgurl"));
+       Amount.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               float quantity=Integer.parseInt(Amount.getText().toString());
+               if(Amount!=null){
+                   Intent intent = getIntent();
+                   float unitprice = intent.getFloatExtra("price",0);
+                   float newprice=quantity*unitprice;
+                   TextView newpri = (TextView) findViewById(R.id.tvnewpris);
+                   newpri.setText("Total Price:Rs."+newprice);
+
+               }else{
+                   Toast.makeText(Details.this,"Enter quantity value", Toast.LENGTH_SHORT).show();
+               }
+
+           }
+       });
     }
 }
